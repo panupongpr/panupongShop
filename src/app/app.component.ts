80ec388723412d6cc40ef032c852795185b090ed
ton <panupong.pspd@gmail.com>
2018-12-10 13:09:35 +0700

    Add Component to Main App

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
+import { from } from 'rxjs';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'PanupongShop';
+export class AppComponent implements OnInit{
+  
+  
+// Explicit
+private mainTitle: string;
+constructor() {}
+
+      ngOnInit(): void {
+  
+      this.mainTitle = 'This is app Component'
+ 
 }
+
+
+}// Main Class
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CategoryComponent } from './components/category/category.component';
+import { PromoteComponent } from './components/promote/promote.component';
+import { InfoComponent } from './components/info/info.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CategoryComponent,
+    PromoteComponent,
+    InfoComponent
   ],
   imports: [
     BrowserModule,
